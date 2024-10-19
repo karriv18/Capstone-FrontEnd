e5f21f315e017c36103cfc79ab75876b0ef95497
karriv18 <krlclarence@gmail.com>
2024-10-19 08:04:15 +0800

    Keyboard avoid added

--- Mobile/Emergton/Screens/Authentication/Login.jsx
+++ Mobile/Emergton/Screens/Authentication/Login.jsx
@@ -16,64 +16,66 @@ import {
     TextLinkContent
 } from '../../components/styles';
 import TextInput from '@/components/UserInputs/TextInput';
-
+import KeyboardAvoid from '../../components/KeyboardAvoid'
 const Login = ({ navigation }) => {
     const [hidePassword, setHidePassword] = useState(true);
     return (
-        <StyledContainer>
-            <StatusBar style="dark" />
-            <InnerContainer>
-                <PageLogo name="phone-in-talk" />
-                <PageTitle>EMERGTON</PageTitle>
-                <SubTitle>EMERGENCY APP</SubTitle>
+        <KeyboardAvoid>
+            <StyledContainer>
+                <StatusBar style="dark" />
+                <InnerContainer>
+                    <PageLogo name="phone-in-talk" />
+                    <PageTitle>EMERGTON</PageTitle>
+                    <SubTitle>EMERGENCY APP</SubTitle>
 
-                <Formik
-                    initialValues={{ email: '', password: '' }}
-                    onSubmit={(values) => {
-                        console.log(values)
-                    }}
-                >
-                    {({ handleChange, handleBlur, handleSubmit, values }) => (
-                        <StyledFormArea>
-                            <TextInput
-                                label="Email Address"
-                                icon="email"
-                                placeholder="john@example.com"
-                                onChangeText={handleChange('email')}
-                                onBlur={handleBlur('email')}
-                                value={values.email}
-                                keyboardType="email-address"
-                            />
-                            <TextInput
-                                label="Password"
-                                icon="lock-person"
-                                placeholder="********"
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                value={values.password}
-                                isPassword={true}
-                                hidePassword={hidePassword}
-                                setHidePassword={setHidePassword}
-                                secureTextEntry={hidePassword}
-                            />
-                            <StyledButton onPress={handleSubmit}>
-                                <ButtonText>
-                                    Login
-                                </ButtonText>
-                            </StyledButton>
-                            <ExtraView>
-                                <ExtraText>Don't have an account? </ExtraText>
-                                <TextLink onPress={() => navigation.push('SignUp')}>
-                                    <TextLinkContent>
-                                        Signup
-                                    </TextLinkContent>
-                                </TextLink>
-                            </ExtraView>
-                        </StyledFormArea>
-                    )}
-                </Formik>
-            </InnerContainer>
-        </StyledContainer>
+                    <Formik
+                        initialValues={{ email: '', password: '' }}
+                        onSubmit={(values) => {
+                            console.log(values)
+                        }}
+                    >
+                        {({ handleChange, handleBlur, handleSubmit, values }) => (
+                            <StyledFormArea>
+                                <TextInput
+                                    label="Email Address"
+                                    icon="email"
+                                    placeholder="john@example.com"
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                    value={values.email}
+                                    keyboardType="email-address"
+                                />
+                                <TextInput
+                                    label="Password"
+                                    icon="lock-person"
+                                    placeholder="********"
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    value={values.password}
+                                    isPassword={true}
+                                    hidePassword={hidePassword}
+                                    setHidePassword={setHidePassword}
+                                    secureTextEntry={hidePassword}
+                                />
+                                <StyledButton onPress={handleSubmit}>
+                                    <ButtonText>
+                                        Login
+                                    </ButtonText>
+                                </StyledButton>
+                                <ExtraView>
+                                    <ExtraText>Don't have an account? </ExtraText>
+                                    <TextLink onPress={() => navigation.push('SignUp')}>
+                                        <TextLinkContent>
+                                            Signup
+                                        </TextLinkContent>
+                                    </TextLink>
+                                </ExtraView>
+                            </StyledFormArea>
+                        )}
+                    </Formik>
+                </InnerContainer>
+            </StyledContainer>
+        </KeyboardAvoid>
     );
 }
 
--- Mobile/Emergton/Screens/Authentication/SignUp.jsx
+++ Mobile/Emergton/Screens/Authentication/SignUp.jsx
@@ -16,89 +16,106 @@ import {
     TextLinkContent
 } from '../../components/styles';
 import TextInput from '@/components/UserInputs/TextInput';
-
+import KeyboardAvoid from '@/components/KeyboardAvoid';
 const Signup = ({ navigation }) => {
     const [hidePassword, setHidePassword] = useState(true);
+    const [hideConfirmPassword, setHideConfirmPassword] = useState(true);
     return (
-        <StyledContainer>
-            <StatusBar style="dark" />
-            <InnerContainer>
-                <PageTitle>SIGN UP </PageTitle>
-                <SubTitle>Create an Account</SubTitle>
+        <KeyboardAvoid>
 
-                <Formik
-                    initialValues={{ fullname: '', email: '', password: '', address: '', landmark: '' }}
-                    onSubmit={(values) => {
-                        console.log(values)
-                    }}
-                >
-                    {({ handleChange, handleBlur, handleSubmit, values }) => (
-                        <StyledFormArea>
-                            <TextInput
-                                label="Email Address"
-                                icon="email"
-                                placeholder="john@example.com"
-                                onChangeText={handleChange('email')}
-                                onBlur={handleBlur('email')}
-                                value={values.email}
-                                keyboardType="email-address"
-                            />
-                            <TextInput
-                                label="Full Name"
-                                icon="drive-file-rename-outline"
-                                placeholder="Full Name"
-                                onChangeText={handleChange('fullname')}
-                                onBlur={handleBlur('fullname')}
-                                value={values.fullname}
-                                keyboardType="email-address"
-                            />
-                            <TextInput
-                                label="Password"
-                                icon="lock-person"
-                                placeholder="********"
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                value={values.password}
-                                isPassword={true}
-                                hidePassword={hidePassword}
-                                setHidePassword={setHidePassword}
-                                secureTextEntry={hidePassword}
-                            />
-                            <TextInput
-                                label="Landmark"
-                                icon="landscape"
-                                placeholder="Landmark"
-                                onChangeText={handleChange('landmark')}
-                                onBlur={handleBlur('fullname')}
-                                value={values.landmark}
-                            />
-                            <TextInput
-                                label="Address"
-                                icon="place"
-                                placeholder="Address"
-                                onChangeText={handleChange('address')}
-                                onBlur={handleBlur('address')}
-                                value={values.address}
-                            />
-                            <StyledButton onPress={handleSubmit}>
-                                <ButtonText>
-                                    Sign Up
-                                </ButtonText>
-                            </StyledButton>
-                            <ExtraView>
-                                <ExtraText>Already have an Account? </ExtraText>
-                                <TextLink onPress={() => navigation.push('Login')}>
-                                    <TextLinkContent>
-                                        Login
-                                    </TextLinkContent>
+            <StyledContainer>
+                <StatusBar style="dark" />
+                <InnerContainer>
+                    <PageTitle>SIGN UP </PageTitle>
+                    <SubTitle>Create an Account</SubTitle>
 
-                                </TextLink>
-                            </ExtraView>
-                        </StyledFormArea>
-                    )}
-                </Formik>
-            </InnerContainer>
-        </StyledContainer>
+                    <Formik
+                        initialValues={{ fullname: '', email: '', password: '', confirmPassword: '', address: '', landmark: '' }}
+                        onSubmit={(values) => {
+                            console.log(values)
+                        }}
+                    >
+                        {({ handleChange, handleBlur, handleSubmit, values }) => (
+                            <StyledFormArea>
+                                <TextInput
+                                    label="Email Address"
+                                    icon="email"
+                                    placeholder="john@example.com"
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                    value={values.email}
+                                    keyboardType="email-address"
+                                />
+                                <TextInput
+                                    label="Full Name"
+                                    icon="drive-file-rename-outline"
+                                    placeholder="Full Name"
+                                    onChangeText={handleChange('fullname')}
+                                    onBlur={handleBlur('fullname')}
+                                    value={values.fullname}
+                                    keyboardType="email-address"
+                                />
+                                <TextInput
+                                    label="Password"
+                                    icon="lock-person"
+                                    placeholder="********"
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    value={values.password}
+                                    isPassword={true}
+                                    hidePassword={hidePassword}
+                                    setHidePassword={setHidePassword}
+                                    secureTextEntry={hidePassword}
+                                />
+                                <TextInput
+                                    label="Confirm password"
+                                    icon="lock-person"
+                                    placeholder="********"
+                                    onChangeText={handleChange('confirmPassword')}
+                                    onBlur={handleBlur('confirmPassword')}
+                                    value={values.confirmPassword}
+                                    isPassword={true}
+                                    hidePassword={hideConfirmPassword}
+                                    setHidePassword={setHideConfirmPassword}
+                                    secureTextEntry={hideConfirmPassword}
+                                />
+                                <TextInput
+                                    label="Landmark"
+                                    icon="landscape"
+                                    placeholder="Landmark"
+                                    onChangeText={handleChange('landmark')}
+                                    onBlur={handleBlur('fullname')}
+                                    value={values.landmark}
+                                />
+                                <TextInput
+                                    label="Address"
+                                    icon="place"
+                                    placeholder="Address"
+                                    onChangeText={handleChange('address')}
+                                    onBlur={handleBlur('address')}
+                                    value={values.address}
+                                />
+                                <StyledButton onPress={handleSubmit}>
+                                    <ButtonText>
+                                        Sign Up
+                                    </ButtonText>
+                                </StyledButton>
+                                <ExtraView>
+                                    <ExtraText>Already have an Account? </ExtraText>
+                                    <TextLink onPress={() => navigation.push('Login')}>
+                                        <TextLinkContent>
+                                            Login
+                                        </TextLinkContent>
+
+                                    </TextLink>
+                                </ExtraView>
+                            </StyledFormArea>
+                        )}
+                    </Formik>
+                </InnerContainer>
+            </StyledContainer>
+        </KeyboardAvoid>
+
     );
 }
 
